25832d4b8c85847a5e3298743fa4b744f79893cf
Heliandro <heliandro.l@gmail.com>
2022-03-13 21:43:00 -0300

    refactor(topico): aplica a notacao PageableDefault para definir os queryParams com valores default

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -35,11 +36,8 @@ public class TopicosController {
     @GetMapping
     public Page<TopicoDto> lista(
             @RequestParam(required = false) String nomeCurso,
-            @RequestParam int pagina,
-            @RequestParam int qtd,
-            @RequestParam(required = false) String ordenacao
+            @PageableDefault(sort = "id", direction = Sort.Direction.DESC, page = 0, size = 10) Pageable paginacao
     ) {
-        Pageable paginacao = PageRequest.of(pagina, qtd, Sort.Direction.DESC, ordenacao);
 
         if (nomeCurso == null) {
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
